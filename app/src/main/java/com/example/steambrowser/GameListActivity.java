108cc5c1b3e35658220ce10068fa61db89a33df1
sleek-turtle <reach.maddaloc@gmail.com>
2019-02-26 17:34:14 -0800

    added drawer to list result activity

--- app/src/main/java/com/example/steambrowser/GameListActivity.java
+++ app/src/main/java/com/example/steambrowser/GameListActivity.java
@@ -1,13 +1,22 @@
 package com.example.steambrowser;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 
 import java.util.ArrayList;
 
-public class GameListActivity extends AppCompatActivity {
+public class GameListActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     private static final ArrayList<String> dummyGameList = new ArrayList<String>() {{ // dummy data
         add("GAME 1");
@@ -35,11 +44,47 @@ public class GameListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         mGameListRecyclerView = findViewById(R.id.rv_game_list);
         mGameListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mGameListRecyclerView.setHasFixedSize(true);
 
         mGameListAdapter = new GameListAdapter();
         mGameListRecyclerView.setAdapter(mGameListAdapter);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+
+        switch(id){
+            case R.id.dummy_game_list:
+                Intent gameListIntent = new Intent(this, GameListActivity.class);
+                startActivity(gameListIntent);
+                return true;
+            default:
+                break;
+
+        }
+        return true;
     }
 }
