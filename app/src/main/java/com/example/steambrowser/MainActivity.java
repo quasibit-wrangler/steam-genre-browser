b1978e3669023938def44fbe9dcd9b8fadf1079b
kelliulep <kelliann.ulep@gmail.com>
2019-03-01 16:17:09 -0800

    Re-added drawer (no hamburger)

--- app/src/main/java/com/example/steambrowser/MainActivity.java
+++ app/src/main/java/com/example/steambrowser/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.steambrowser;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.MenuItem;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -53,40 +56,39 @@ public class MainActivity extends AppCompatActivity
         ItemTouchHelper helper = new ItemTouchHelper(callback);
         helper.attachToRecyclerView(recyclerView);
     }
-//
-//    @Override
-//    public void onBackPressed() {
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        if (drawer.isDrawerOpen(GravityCompat.START)) {
-//            drawer.closeDrawer(GravityCompat.START);
-//        } else {
-//            super.onBackPressed();
-//        }
-//    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
 
 
     // TODO: put in drawer later
-//    @SuppressWarnings("StatementWithEmptyBody")
-//    @Override
-//    public boolean onNavigationItemSelected(MenuItem item) {
-//        // Handle navigation view item clicks here.
-//        int id = item.getItemId();
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        drawer.closeDrawer(GravityCompat.START);
-//
-//
-//        switch(id){
-//            case R.id.dummy_game_list:
-//                Intent gameListIntent = new Intent(this, GameListActivity.class);
-//                startActivity(gameListIntent);
-//                return true;
-//            default:
-//                break;
-//
-//        }
-//        return true;
-//    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+
+        switch(id){
+            case R.id.dummy_game_list:
+                Intent gameListIntent = new Intent(this, GameListActivity.class);
+                startActivity(gameListIntent);
+                return true;
+            default:
+                break;
+
+        }
+        return true;
+    }
 
     @Override
     public void onClick(View view) {
