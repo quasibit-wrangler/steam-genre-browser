6f99a2fd12d885af1472c223e90a251b935a5cbc
MaryJacobsen <maryevelynjacobsen@gmail.com>
2019-03-13 18:24:26 -0700

    more with prefs

--- app/src/main/java/com/example/steambrowser/GameListActivity.java
+++ app/src/main/java/com/example/steambrowser/GameListActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.preference.PreferenceManager;
+import android.content.SharedPreferences;
 
 import java.util.ArrayList;
 
